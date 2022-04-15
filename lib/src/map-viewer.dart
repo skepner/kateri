05920520248d5a3cc613d1d12a8af5075dcf897e
Eugene Skepner <github@skepner.eu>
2022-04-15 09:55:57 +0200

    map viewer widget

--- lib/src/map-viewer.dart
+++ lib/src/map-viewer.dart
@@ -38,14 +38,7 @@ class _AntigenicMapViewWidgetState extends State<AntigenicMapViewWidget> {
   late double aspectRatio;
   late double borderWidth;
   late Color borderColor;
-  late AntigenicMapPainter antigenicMapPainter;
-
-  // void setColor(Color color) {
-  //   setState(() {
-  //     borderColor = color;
-  //     print("borderColor $borderColor");
-  //   });
-  // }
+  // late AntigenicMapPainter antigenicMapPainter;
 
   @override
   void initState() {
@@ -55,12 +48,13 @@ class _AntigenicMapViewWidgetState extends State<AntigenicMapViewWidget> {
     borderWidth = widget.borderWidth;
     borderColor = widget.borderColor;
 
+    // antigenicMapPainter = AntigenicMapPainter(this);
     chart = Chart(localPath: "/r/h1pdm-hi-turkey-vidrl.chain.ace");
-    antigenicMapPainter = AntigenicMapPainter(chart);
   }
 
   @override
   Widget build(BuildContext context) {
+    print("_AntigenicMapViewWidgetState.build");
     return Container(
         // margin: const EdgeInsets.all(10.0),
         decoration: BoxDecoration(border: Border.all(color: borderColor, width: borderWidth)),
@@ -74,7 +68,7 @@ class _AntigenicMapViewWidgetState extends State<AntigenicMapViewWidget> {
                 body: Stack(children: <Widget>[
                   // Column(children: [Container(color: orange, child: Text("path: $path")), Container(color: Colors.green, child: Text("path: $path"))]),
                   // Center(child: Container(color: borderColor /*  Colors.orange */, child: Text("path: $path $borderColor"))),
-                  CustomPaint(painter: antigenicMapPainter, size: const Size(99999, 99999)),
+                  CustomPaint(painter: AntigenicMapPainter(chart), size: const Size(99999, 99999)),
                   Positioned(
                       left: 0,
                       top: 0,
@@ -84,6 +78,34 @@ class _AntigenicMapViewWidgetState extends State<AntigenicMapViewWidget> {
                       ))
                 ]))));
   }
+
+  // ----------------------------------------------------------------------
+
+  void openAceFile() async {
+    final file = (await FilePicker.platform.pickFiles())?.files.single;
+
+    // accesing file?.path on web always reports an error (regardles of using try/catch)
+    if (file?.bytes != null) {
+      setChart(Chart(bytes: file?.bytes));
+    } else {
+      setChart(Chart(localPath: file?.path));
+    }
+  }
+
+  void setChart(Chart? newChart) {
+    setState(() {
+      chart = newChart;
+      // antigenicMapPainter.chartUpdated();
+    });
+  }
+
+  // void setColor(Color color) {
+  //   setState(() {
+  //     borderColor = color;
+  //     print("borderColor $borderColor");
+  //   });
+  // }
+
 }
 
 // ----------------------------------------------------------------------
@@ -96,11 +118,12 @@ class AntigenicMapViewWidgetMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(padding: EdgeInsets.zero, children: [
-        ListTile(
+      ListTile(
           leading: const Icon(Icons.file_open_rounded),
           title: const Text("Open"),
           onTap: () {
             Navigator.pop(context);
+            antigenicMapViewWidgetState.openAceFile();
           }),
       ListTile(
           leading: const Icon(Icons.picture_as_pdf_rounded),
@@ -145,47 +168,40 @@ class AntigenicMapViewWidgetMenu extends StatelessWidget {
 // ----------------------------------------------------------------------
 
 class AntigenicMapPainter extends CustomPainter {
-  Chart? chart;
-  late ChartViewer viewer;
+  final ChartViewer viewer;
 
-  AntigenicMapPainter([Chart? chart]) {
-    setChart(chart);
-  }
-
-  void setChart(Chart? chart) {
-    this.chart = chart;
-    viewer = ChartViewer(chart);
-  }
+  AntigenicMapPainter(Chart? chart) : viewer = ChartViewer(chart);
 
   @override
   void paint(Canvas canvas, Size size) {
     final stopwatch = Stopwatch()..start();
     viewer.paint(CanvasFlutter(canvas, size));
-    print("[paint] ${chart?.antigens.length}:${chart?.sera.length} ${stopwatch.elapsed} -> ${1e6 / stopwatch.elapsedMicroseconds} frames per second");
+    // print("[paint] ${chart?.antigens.length}:${chart?.sera.length} ${stopwatch.elapsed} -> ${1e6 / stopwatch.elapsedMicroseconds} frames per second");
+    print("[paint] ${stopwatch.elapsed} -> ${1e6 / stopwatch.elapsedMicroseconds} frames per second");
   }
 
-  void exportPdf({bool open = true}) async {
-    if (chart != null) {
-      final bytes = await viewer.exportPdf();
-      if (bytes != null) {
-        final filename = await FileSaver.instance.saveFile(chart!.info.nameForFilename(), bytes, "pdf", mimeType: MimeType.PDF);
-        if (open && UniversalPlatform.isMacOS) {
-          await Process.run("open-and-back-to-emacs", [filename]);
-        }
-      }
-    }
-  }
+  // void exportPdf({bool open = true}) async {
+  //   if (chart != null) {
+  //     final bytes = await viewer.exportPdf();
+  //     if (bytes != null) {
+  //       final filename = await FileSaver.instance.saveFile(chart!.info.nameForFilename(), bytes, "pdf", mimeType: MimeType.PDF);
+  //       if (open && UniversalPlatform.isMacOS) {
+  //         await Process.run("open-and-back-to-emacs", [filename]);
+  //       }
+  //     }
+  //   }
+  // }
 
-  void openAceFile() async {
-    final file = (await FilePicker.platform.pickFiles())?.files.single;
+  // void openAceFile() async {
+  //   final file = (await FilePicker.platform.pickFiles())?.files.single;
 
-    // accesing file?.path on web always reports an error (regardles of using try/catch)
-    if (file?.bytes != null) {
-      setChart(Chart(bytes: file?.bytes));
-    } else {
-      setChart(Chart(localPath: file?.path));
-    }
-  }
+  //   // accesing file?.path on web always reports an error (regardles of using try/catch)
+  //   if (file?.bytes != null) {
+  //     setChart(Chart(bytes: file?.bytes));
+  //   } else {
+  //     setChart(Chart(localPath: file?.path));
+  //   }
+  // }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
